7e521da653bd5393836f0bcee0715859a1d93fe4
Magen Bouchard <Magen.Bouchard@gmail.com>
2020-02-21 02:14:16 -0500

    Updates

---
+++ precache-manifest.85c52994ae671eee79ed56648bdce159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726fbe45bb910bcf3da9443ccfe0cb04",
+    "url": "/newportfolio/index.html"
+  },
+  {
+    "revision": "a58d23be9a69d804b43b",
+    "url": "/newportfolio/static/css/main.7c74ab6b.chunk.css"
+  },
+  {
+    "revision": "23e8d450756ee838f7b2",
+    "url": "/newportfolio/static/js/2.72b8f1e2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/newportfolio/static/js/2.72b8f1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58d23be9a69d804b43b",
+    "url": "/newportfolio/static/js/main.4e2e51ac.chunk.js"
+  },
+  {
+    "revision": "b8e69bf175b9af8e6756",
+    "url": "/newportfolio/static/js/runtime-main.b654af23.js"
+  },
+  {
+    "revision": "8d2b95c97fe2310534a20f5189d9ad94",
+    "url": "/newportfolio/static/media/IMG_8267(1).8d2b95c9.JPG"
+  }
+]);
